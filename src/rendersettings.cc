2328b0ed0872bfbefe21970bc7bfbb548573871d
Shaina <shaina@Sabarwal.(none)>
2014-05-30 11:45:36 +0530

    legacyeditor

--- src/rendersettings.cc
+++ src/rendersettings.cc
@@ -3,24 +3,24 @@
 RenderSettings *RenderSettings::inst(bool erase)
 {
 	static RenderSettings *instance = new RenderSettings;
-	if (erase) {
+/*	if (erase) {
 		delete instance;
 		instance = NULL;
 	}
-	return instance;
+*/	return instance;
 }
 
 RenderSettings::RenderSettings()
 {
 	openCSGTermLimit = 100000;
 	far_gl_clip_limit = 100000.0;
-	img_width = 512;
+	img_width = 300;
 	img_height = 512;
 	this->colors[BACKGROUND_COLOR] = Color4f(0xff, 0xff, 0xe5);
-	this->colors[OPENCSG_FACE_FRONT_COLOR] = Color4f(0xf9, 0xd7, 0x2c);
-	this->colors[OPENCSG_FACE_BACK_COLOR] = Color4f(0x9d, 0xcb, 0x51);
-	this->colors[CGAL_FACE_FRONT_COLOR] = Color4f(0xf9, 0xd7, 0x2c);
-	this->colors[CGAL_FACE_BACK_COLOR] = Color4f(0x9d, 0xcb, 0x51);
+	this->colors[OPENCSG_FACE_FRONT_COLOR] = Color4f(0xff, 0x00, 0x00);
+	this->colors[OPENCSG_FACE_BACK_COLOR] = Color4f(0xff, 0x00, 0x00);
+	this->colors[CGAL_FACE_FRONT_COLOR] = Color4f(0xff, 0x00, 0x00);
+	this->colors[CGAL_FACE_BACK_COLOR] = Color4f(0xff, 0x00, 0x00);
 	this->colors[CGAL_FACE_2D_COLOR] = Color4f(0x00, 0xbf, 0x99);
 	this->colors[CGAL_EDGE_FRONT_COLOR] = Color4f(0xff, 0x00, 0x00);
 	this->colors[CGAL_EDGE_BACK_COLOR] = Color4f(0xff, 0x00, 0x00);
